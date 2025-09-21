a771f0bf7e6e35fde2fff022e57ec42b567534f4
Qwqwww <jollot@yandex.ru>
2025-09-21 17:58:54 +0700

    Добавлена первая версия html-странички из лекции

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -7,6 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "Hello!"
+
+
+@app.route("/login/", methods=["get", "post"])
+def login():
+    return render_template("login.html")
 
 
 app.run()
