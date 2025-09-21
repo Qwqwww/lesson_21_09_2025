f597eefca14294a6dbd00dd9478fb98e6474a67c
Qwqwww <jollot@yandex.ru>
2025-09-21 18:07:26 +0700

    Добавлена обработка значений с формы

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -12,6 +12,14 @@ def hello():
 @app.route("/login/", methods=["get", "post"])
 def login():
     message = "Место под сообщение"
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username != "123":
+            message = "Неправильное имя пользователя"
+        else:
+            message = f"Имя пользователя: {username}, пароль {password}"
     return render_template("login.html", message=message)
 
 
